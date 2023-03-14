7cf2dc4a2039c47f42cedb703ed5822386e720d0
js4183 <artomes@naver.com>
2023-03-14 16:18:32 +0900

    first commit

---
+++ src/main/java/com/example/FYM/FymApplication.java
@@ -0,0 +1,13 @@
+package com.example.FYM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FYM/FymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FYM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
